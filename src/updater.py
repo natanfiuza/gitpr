d3167ade6704cf3440b9cba20aa6f254d72ad760
Nataniel Fiuza <natan.fiuza@gmail.com>
2026-04-26 13:21:28 -0300

    Atualiza a versão para 0.0.5

--- src/updater.py
+++ src/updater.py
@@ -6,7 +6,7 @@ import shutil
 import click
 
 # Versão atual do seu executável local (Atualize isso a cada novo build!)
-__version__ = "0.0.4"
+__version__ = "0.0.5"
 GITHUB_API_URL = "https://api.github.com/repos/natanfiuza/gitpr/releases/latest"
 
 def get_gitpr_dir():
